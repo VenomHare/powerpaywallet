7ac7d9b9ee763e2f1789f761a6ee6700ae53fc10
Sarthak <sarthakkadam147@gmail.com>
2025-08-09 13:27:00 +0000

    fix(core): added cors

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,11 +1,19 @@
 import express, { Application } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import v1Router from "./v1";
 
 dotenv.config();
 const PORT = process.env.WEBHOOK_PORT || 3002;
 
-const app : Application = express();
+const app: Application = express();
+app.use(cors({
+    origin: [
+        'http://localhost:3003',
+        'https://powerpay-mock-bank.venomhare.space'
+    ],
+    credentials: true
+}))
 app.use(express.json());
 app.use((req, _, next) => {
     console.log([req.method, req.url, JSON.stringify(req.body)].join(" "));
